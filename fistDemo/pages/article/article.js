1bf5ac3667a124ede32b252bdad17fd4e133a218
Bessi <425782059@qq.com>
2019-05-08 17:43:58 +0800

    Complete the magazine page

---
+++ fistDemo/pages/article/article.js
@@ -0,0 +1,128 @@
+// pages/article/article.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    listLike: {
+      0: true,
+      1: false,
+      2: false,
+      3: true
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getData();
+    this.getLikeData();
+    
+  },
+  moreTap: function(e){
+    var type = e.currentTarget.dataset.articletype;
+    wx.showActionSheet({
+      itemList: ['内容过期了','内容和'+type+'不相关','不再显示'+type+'相关内容'],
+      itemColor: '#333'
+    })
+  },
+  onlikeTap: function(e){
+    var index = e.currentTarget.dataset.articleindex;
+    var listLike = this.data.listLike;
+    var isLike = !listLike[index];
+    listLike[index] = !isLike;
+    this.setData({
+      listLike: listLike
+    })
+    wx.setStorageSync('listLike', listLike);
+  },
+  getData: function(){
+    var that = this;
+    wx.request({
+      url:'https://easy-mock.com/mock/5bb8c1c63ccc501a316e3ccb/magazine/getRecommendInfo',
+      success: function(res){
+        // console.log(res)
+        that.setData({
+          recommend: res.data.data
+        })
+      }
+    })
+    wx.request({
+      url:'https://easy-mock.com/mock/5bb8c1c63ccc501a316e3ccb/magazine/getMarkTypeList',
+      success: function(res){
+        // console.log(res)
+        that.setData({
+          markType: res.data.data
+        })
+      }
+    })
+    wx.request({
+      url:'https://easy-mock.com/mock/5bb8c1c63ccc501a316e3ccb/magazine/getHomeArticleList',
+      success: function(res){
+        // console.log(res.data.data)
+        that.setData({
+          articleList: res.data.data
+        })
+      }
+    })
+  },
+  getLikeData: function(){
+    var listLikeStorage = wx.getStorageSync("listLike");
+    if(!listLikeStorage){
+      listLikeStorage = {}
+    }
+    this.setData({
+      listLike: listLikeStorage
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
