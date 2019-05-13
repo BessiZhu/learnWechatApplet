b0585dd34798d0ca31ea5c3b427602375530125b
Bessi <425782059@qq.com>
2019-05-13 17:40:36 +0800

    Carousel Graph and Video Player Component Completion

--- fistDemo/pages/articleInfoDetail/articleInfoDetail.js
+++ fistDemo/pages/articleInfoDetail/articleInfoDetail.js
@@ -1,20 +1,54 @@
 // pages/articleInfoDetail/articleInfoDetail.js
+var request = require('../../utils/request.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    coverHidden: false,
+    playing:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    this.getData(options.articleId);
+    this.audioPlayTap();
   },
-
+  getData: function (options) {
+    var that = this;
+    request({
+      url: '/getArticleDetail/' + options,
+      success: function (res) {
+        // console.log(res)
+        that.setData({
+          articleDetail: res
+        })
+      }
+    })
+  },
+  videoPlayTap: function () {
+    this.setData({
+      coverHidden: true
+    })
+    this.videoCtx = wx.createVideoContext('myVideo');
+    this.videoCtx.play();
+  },
+  audioPlayTap: function () {
+    var audio = wx.getBackgroundAudioManager();
+    audio.title = '此时此刻';
+    audio.epname = '此时此刻';
+    audio.singer = '许巍';
+    audio.coverImgUrl = 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000';
+    audio.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46';
+    console.log(this.playing)
+    this.setData({
+      playing: !this.playing
+    })
+    console.log(this.playing)
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
